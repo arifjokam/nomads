208e3b52493f7460c6f1ac284bd35f613f5e276f
arifjokam <arif.hidayatulloh313@gmail.com>
2021-02-01 12:38:56 +0700

    edit workflow dan home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -26,19 +26,19 @@ NOMADS
   <div class="container">
     <section class="section-stats row justify-content-center" id="stats">
       <div class="col-3 col-md-2 stats-detail">
-        <h2>25K</h2>
+        <h2>200K</h2>
         <p>Members</p>
       </div>
       <div class="col-3 col-md-2 stats-detail">
-        <h2>24</h2>
+        <h2>30</h2>
         <p>Countries</p>
       </div>
       <div class="col-3 col-md-2 stats-detail">
-        <h2>10K</h2>
+        <h2>20K</h2>
         <p>Hotel</p>
       </div>
       <div class="col-3 col-md-2 stats-detail">
-        <h2>100</h2>
+        <h2>500</h2>
         <p>Partners</p>
       </div>
     </section>
